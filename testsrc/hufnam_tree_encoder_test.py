904d53881ffb08c0cc2f1f044ae8a7493c1d9eff
Serhii_Baidin <Serhii_Baidin@epam.com>
2019-05-21 12:48:56 +0300

    tests

--- testsrc/hufnam_tree_encoder_test.py
+++ testsrc/hufnam_tree_encoder_test.py
@@ -1,19 +1,28 @@
+from io import BytesIO
 from unittest import TestCase
 
 from src.hufman.dto.tree_node import TreeNode
+from src.hufman.hufman_tree_encoder import tree_to_io, tree_from_io
 
 
 class HufmanTreeEncoderTest(TestCase):
     def setUp(self) -> None:
         self.tree = TreeNode(
             left_child=TreeNode(
-                left_child=TreeNode(character=65535),
-                right_child=TreeNode(character=65534)
+                left_child=TreeNode(character=255),
+                right_child=TreeNode(character=254)
             ),
             right_child=TreeNode(
                 left_child=TreeNode(
-                    left_child=TreeNode(character=65533),
-                    right_child=TreeNode(character=65532)
+                    left_child=TreeNode(character=253),
+                    right_child=TreeNode(character=252)
                 )
             )
         )
+
+    def test_should_encode_and_read_tree_without_loosing_data(self):
+        virtual_file = BytesIO()
+        tree_to_io(self.tree, virtual_file)
+        virtual_file.seek(0)
+        readed = tree_from_io(virtual_file)
+        self.assertEqual(self.tree, readed)
